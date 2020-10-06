7d8bda6f917c74dc5c8dd64ecbd2c8385d933154
SebastianSinozic <55134663+SebastianSinozic@users.noreply.github.com>
2020-10-06 11:03:42 +0200

    Add files via upload

---
+++ sinozicSebastianStatickeVarijable2/sinozicSebastianStatickeVarijable2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinozicSebastianStatickeVarijable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa X = new PrvaKlasa("Pero","Perić");
+            Console.WriteLine(X.ToString());
+            PrvaKlasa Y = new PrvaKlasa("Ivica","Ivić");
+            Console.WriteLine(Y.ToString());
+            PrvaKlasa Z = new PrvaKlasa();
+            
+
+            Z.Ime = "Milorad";
+            Z.Prezime = " Milić";
+            Console.WriteLine(Z.ToString());
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
---
+++ sinozicSebastianStatickeVarijable2/sinozicSebastianStatickeVarijable2/PrvaKlasa.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sinozicSebastianStatickeVarijable2
+{
+    class PrvaKlasa
+    {
+        private static int brojac;
+
+        string ime; 
+        string prezime;
+     
+        
+        public PrvaKlasa()
+        {
+            brojac++;
+
+        }
+        public PrvaKlasa(string ime, string prezime)
+        {
+            brojac++;
+            this.ime = ime;
+            this.prezime = prezime;
+
+        }
+
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+
+        
+        public int Brojac() 
+        {
+            return brojac;
+
+            
+        }
+        public override string ToString()
+        {
+            string ispis = "Redni broj" + brojac+ "," +ime +" " + prezime;
+      
+            
+            return ispis;
+            
+            
+        }
+
+
+    }
+}
